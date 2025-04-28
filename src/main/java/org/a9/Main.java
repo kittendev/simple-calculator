401f38fb1b60087cb0634a068fa65e3d0defaac4
KittenDev <Dafaalfarizki33.DA@gmail.com>
2025-04-28 13:02:29 +0700

    sedikit modifikasi biar keren

--- src/main/java/org/a9/Main.java
+++ src/main/java/org/a9/Main.java
@@ -9,13 +9,13 @@ public class Main {
         System.out.println("Kalkulator Sederhana");
         System.out.println("---------------------");
 
-        double num1 = InputValidator.getNumber("Masukkan angka pertama: ");
-        String operator = InputValidator.getOperator();
-        double num2 = InputValidator.getNumber("Masukkan angka kedua: ");
-
-        double result = 0;
-
         try {
+            double num1 = InputValidator.getNumber("Masukkan angka pertama: ");
+            String operator = InputValidator.getOperator();
+            double num2 = InputValidator.getNumber("Masukkan angka kedua: ");
+
+            double result = 0;
+
             switch (operator) {
                 case "+":
                     result = calculator.add(num1, num2);
@@ -33,8 +33,8 @@ public class Main {
                     break;
             }
 
-            System.out.println("Hasil: " + result);
-        } catch (ArithmeticException e) {
+            System.out.println("Hasil: " + (result == (int) result ? (int) result : String.format("%.5f", result)));
+        } catch (ArithmeticException | IllegalArgumentException e) {
             System.out.println("Error: " + e.getMessage());
         }
 
