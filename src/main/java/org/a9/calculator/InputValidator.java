ab739da2cd7f047faa0ea23779212ecf68c26ae8
firmanzz <124325264+firmanzz@users.noreply.github.com>
2025-05-03 19:44:38 +0700

    benerin validator sama validatorTest V4

--- src/main/java/org/a9/calculator/InputValidator.java
+++ src/main/java/org/a9/calculator/InputValidator.java
@@ -2,8 +2,28 @@ package org.a9.calculator;
 
 import java.util.Scanner;
 
+/**
+ * Kelas ini merupakan kelas yang berfumgsi untuk menerima input angka dan operator. Kelas ini juga
+ * berfungsi sebagai validator input agar sesuai dengan spesifikasi program.
+ *
+ * @version 1.0
+ * @since 2025-04-27
+ * @author Firman
+ */
+
 public class InputValidator {
 
+    InputValidator () {
+        // Constructor default
+    }
+
+    /**
+     * Metode ini digunakan untuk menerima dan memvalidasi input angka.
+     *
+     * @param scanner untuk menerima input.
+     * @param prompt yang akan ditampilkan ke layar.
+     * @return angka yang diinputkan user.
+     */
     public static double getNumber(Scanner scanner, String prompt) {
         System.out.print(prompt);
         if (!scanner.hasNextDouble()) {
@@ -17,6 +37,13 @@ public class InputValidator {
         return number;
     }
 
+    /**
+     * Metode ini digunakan untuk menerima dan memvalidasi input operator.
+     *
+     * @param scanner untuk menerima input.
+     * @param prompt yang akan ditampilkan ke layar.
+     * @return operator yang diinputkan user.
+     */
     public static String getOperator(Scanner scanner, String prompt) {
         System.out.print(prompt);
         String operator = scanner.next();
