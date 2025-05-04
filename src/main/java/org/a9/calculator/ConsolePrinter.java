59973061cf9290f673dc58252e17ae46a1f1136b
Muhammad Raihan Fasya Mian <132323840+hanssip15@users.noreply.github.com>
2025-05-04 12:23:03 +0700

    [Update] Doc String for ConsolePrinter

--- src/main/java/org/a9/calculator/ConsolePrinter.java
+++ src/main/java/org/a9/calculator/ConsolePrinter.java
@@ -1,5 +1,13 @@
 package org.a9.calculator;
 
+/**
+ * Kelas ini merupakan kelas yang mengatur tampilan yang akan muncul pada console pengguna.
+ *
+ * @version 1.0
+ * @since 2025-04-27
+ * @author Raihan
+ */
+
 public class ConsolePrinter {
     public static void printResult(double result) {
         System.out.println("Hasil: " + (result == (int) result ? (int) result : String.format("%.5f", result)));
